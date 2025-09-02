6f4af7f30997cacea1f657cd2df40b28c0fa2da4
tdavis36 <196705197+tdavis36@users.noreply.github.com>
2025-09-02 17:21:10 -0400

    initial commit

---
+++ src/main/java/com/abetappteam/abetapp/AbetAssessmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.abetappteam.abetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbetAssessmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbetAssessmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abetappteam/abetapp/AbetAssessmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abetappteam.abetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbetAssessmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
