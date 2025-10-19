99615a35d0d2e0f907547146370b5271367c45a3
Caleb Jones (Personal) <cjones76@ycp.edu>
2025-10-19 17:27:05 -0400

    Set up Navbar, Log In, Sign Up component templates and basic styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,32 +21,7 @@
   text-align: center;
 }
 
-header {
-  margin-bottom: 2rem;
-  padding-bottom: 1rem;
-  border-bottom: 1px solid #eee;
-}
-
-nav {
-  margin: 1rem 0;
-}
-
-nav a {
-  margin: 0 1rem;
-  color: #007bff;
-  text-decoration: none;
-}
-
-nav a:hover {
-  text-decoration: underline;
-}
-
-nav a.router-link-active {
-  color: #0056b3;
-  font-weight: bold;
-}
-
-main {
-  margin-top: 2rem;
+header{
+  margin-bottom: 1rem;
 }
 </style>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,10 +3,42 @@
 </script>
 
 <template>
-    <nav>
-        <h1>ABET Assessment App</h1>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/test-connection">Test Connection</router-link>
-    </nav>
+    <div id="navbar">
+        <div id="logo">
+            Abet Assessment App
+        </div>
+        <router-link to="/" class="nav_button">Home</router-link>
+        <router-link to="/test-connection" class="nav_button">Test Connection</router-link>
+        <router-link to="/login" class="nav_button" id="login">Log In</router-link>
+    </div>
     <h1 hidden>This hidden element checks if the site loaded</h1>
 </template>
+
+<style>
+    #navbar {
+        display: flex;
+        width: 80%;
+        margin: auto;
+    }
+
+    #logo{
+        padding: 2rem;
+        border-right: 1px solid black;
+    }
+
+    .nav_button{
+        padding: 2rem;
+        color: black;
+        text-decoration: none;
+    }
+
+    .nav_button:hover{
+        background-color: rgb(200, 200, 200);
+        color: black;
+        text-decoration: none;
+    }
+
+    #login{
+        margin-left: auto;
+    }
+</style>
