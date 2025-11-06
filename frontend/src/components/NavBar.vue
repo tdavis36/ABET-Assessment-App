6382423c3818e29469bb0c405220c471db63c68e
laurenwirt <paigewirt@verizon.net>
2025-11-06 10:21:14 -0500

    fcar, dashboard update
    changed dashboard to homepage, fcar page layout, formatting changes in admin dashboard

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -9,53 +9,45 @@ const isAdmin = true
 
 const emits = defineEmits(["logout"])
 </script>
-
 <template>
-    <div id="navbar">
-        <div id="logo">Abet Assessment App</div>
+  <div id="navbar">
+    <div id="logo">Abet Assessment App</div>
 
-        <router-link to="/" class="nav_button">Home</router-link>
-        <router-link to="/test-connection" class="nav_button">Test Connection</router-link>
+    <router-link to="/" class="nav_button">Home</router-link>
+    <router-link to="/test-connection" class="nav_button">Test Connection</router-link>
 
-        
-        <router-link
-            v-if="loggedIn && isAdmin"
-            to="/admin-dashboard"
-            class="nav_button"
-        >
-            Admin Dashboard
-        </router-link>
+    <!-- Instructor dashboard link only -->
+    <router-link
+      v-if="loggedIn && !isAdmin"
+      to="/instructor-dashboard"
+      class="nav_button"
+    >
+      Instructor Dashboard
+    </router-link>
 
-        <router-link
-            v-else-if="loggedIn && !isAdmin"
-            to="/instructor-dashboard"
-            class="nav_button"
-        >
-            Instructor Dashboard
-        </router-link>
+    <!-- Log In / Log Out -->
+    <router-link
+      v-if="loggedIn"
+      to="/"
+      class="nav_button"
+      id="logout"
+      @click="$emit('logout')"
+    >
+      Logged in as <strong>{{ userID }}</strong> | Log Out
+    </router-link>
 
-    
-        <router-link
-            v-if="loggedIn"
-            to="/"
-            class="nav_button"
-            id="logout"
-            @click="$emit('logout')"
-        >
-            Logged in as <strong>{{ userID }}</strong> | Log Out
-        </router-link>
-
-        <router-link
-            v-else
-            to="/login"
-            class="nav_button"
-            id="login"
-        >
-            Log In
-        </router-link>
-    </div>
-    <h1 hidden>This hidden element checks if the site loaded</h1>
+    <router-link
+      v-else
+      to="/login"
+      class="nav_button"
+      id="login"
+    >
+      Log In
+    </router-link>
+  </div>
+  <h1 hidden>This hidden element checks if the site loaded</h1>
 </template>
+
 
 <style>
 #navbar {
