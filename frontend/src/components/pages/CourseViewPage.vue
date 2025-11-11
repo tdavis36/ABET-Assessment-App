32927777ea920fc073cf2c25a7d92d0eb91b2a15
Caleb Jones (Personal) <cjones76@ycp.edu>
2025-11-11 08:13:22 -0500

    Basic Course View layout

--- frontend/src/components/pages/CourseViewPage.vue
+++ frontend/src/components/pages/CourseViewPage.vue
@@ -4,6 +4,7 @@
 
     const route = useRoute()
 
+    /*
     const course_id = ref(NaN);
     const course_obj = ref({
         id: NaN,
@@ -15,15 +16,33 @@
         is_active: false
     });
     const semester_name = ref('');
+    const instructor_ids = ref([]);
+    const instructor_names = ref(['']);
+    const indicator_ids = ref([])*/
 
+    //--------TEST DATA--------
+    const course_id = ref(1);
+    const course_obj = ref({
+        id: 1,
+        course_code: 'CS101',
+        course_name: 'Fundamentals of Computer Science I',
+        course_description: 'Basic programming concepts and problem solving',
+        semester_id: '1',
+        created_at: '2025-11-10T10:16:56.456221',
+        is_active: true
+    });
+    const semester_name = ref('Fall 2025');
+    const instructor_ids = ref([1, 2]);
+    const indicator_ids = ref([1,2])
+    //--------------------------
     
     async function initialize(){
         course_id.value = parseInt(route.params.course_id as string, 10)
 
+        //Fetch Course data
         try {
             const response = await fetch(`/api/courses/${course_id.value}`);
             const data = await response.json(); // Await the JSON parsing
-            console.log('Fetched JSON data:', data);
             course_obj.value = {
                 id: data.data.id,
                 course_code: data.data.courseCode,
@@ -34,23 +53,101 @@
                 is_active: data.data.isActive
             }
         } catch (error) {
-            console.error('Error fetching or parsing data:', error);
+            console.error('Error fetching or parsing course data:', error);
         }
 
+        //Fetch Semester data
+        /*
+        try {
+            const response = await fetch(`/api/semesters/${course_obj.value.semester_id}`);
+            const data = await response.json(); // Await the JSON parsing
+            console.log('Fetched JSON data:', data);
+            semester_name.value = `${data.data.season} ${data.data.semesterYear}`
+        } catch (error) {
+            console.error('Error fetching or parsing course data:', error);
+        }*/
 
-
+        //Fetch Instructor IDs
+        try {
+            const response = await fetch(`/api/courses/${course_id.value}/instructors`);
+            const data = await response.json(); // Await the JSON parsing
+            instructor_ids.value = data
+        } catch (error) {
+            console.error('Error fetching or parsing course data:', error);
+        }
+        
+        //Fetch Indicator IDs
+        try {
+            const response = await fetch(`/api/courses/${course_id.value}/indicators`);
+            const data = await response.json(); // Await the JSON parsing
+            indicator_ids.value = data
+        } catch (error) {
+            console.error('Error fetching or parsing course data:', error);
+        }
     }
 
-    initialize();
+    //initialize();
     
 </script>
 
 <template>
-    <p>Course ID {{ route.params.course_id }}</p>
-    <p>Course Code: {{ course_obj.course_code }}</p>
-    <p>Course Name: {{ course_obj.course_name }}</p>
-    <p>Course Description: {{ course_obj.course_description }}</p>
-    <p>Semester ID: {{ course_obj.semester_id }}</p>
-    <p>Created At: {{ course_obj.created_at }}</p>
-    <p>Is Active: {{ course_obj.is_active }}</p>
+    <div id="course_body">
+        <div id="course_header">
+            <span id="course_code">{{ course_obj.course_code }}</span>
+            <span id="course_name">{{ course_obj.course_name }}</span>
+        </div>
+        <div id="semester"><a>{{ semester_name }}</a></div>
+        <p id="description">{{ course_obj.course_description }}</p>
+
+        <div id="instructors">
+            Instructors
+            <div v-for="iid in instructor_ids">Placeholder: {{ iid }}</div>
+        </div>
+
+        <div id="performance-indicators">
+            <div v-for="piid in indicator_ids">Placeholder: {{ piid }}</div>
+        </div>
+    </div>
 </template>
+
+<style>
+    #course_body{
+        margin: auto;
+        width: 70%;
+
+        font-family:
+            system-ui,
+            -apple-system,
+            "Segoe UI",
+            Roboto,
+            Helvetica,
+            Arial,
+            sans-serif;
+        text-align: left;
+    }
+
+    #course_header{
+        margin-top: 2rem;
+        margin-bottom: 0.5rem;
+        font-size: 32px;
+    }
+
+    #course_code{
+        color: red;
+        font-weight: bold;
+        margin-right: 1rem;
+    }
+
+    #description{
+        font-style: italic;
+        color: rgb(85, 85, 85);
+    }
+
+    #instructors{
+        margin: 1rem;
+    }
+
+    #performance-indicators{
+        margin: 2rem;
+    }
+</style>
